39893c741512ffe0214d5545ca5a604ffcd0e22f
Rita <148492287+RitaL1990@users.noreply.github.com>
2023-11-01 13:01:56 +0100

    Add files via upload

---
+++ 31-10-23/script.js
@@ -0,0 +1,15 @@
+const animals = [
+    "pappagallo", 
+    "corvo", 
+    "acquila", 
+    "airone", 
+    "struzzo", 
+    "gabbiano", 
+    "falco", 
+    "passero"
+];
+console.log(animals);
+
+for (let i = 0; i <= animals.length - 1; i++) {
+    console.log(animals[i]);
+}
